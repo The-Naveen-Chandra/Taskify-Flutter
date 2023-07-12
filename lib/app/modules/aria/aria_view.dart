1832444377724e4a9c446c65fe210e8d4e562a3e
Naveen Chandra <the.naveenchandra@gmail.com>
2023-07-12 20:09:23 +0530

    Configure Aria CharBot

---
+++ lib/app/modules/aria/aria_view.dart
@@ -0,0 +1,173 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:taskify/app/core/utils/extensions.dart';
+import 'package:taskify/app/modules/aria/widgets/chat_message.dart';
+import 'package:taskify/app/modules/aria/widgets/threedots.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:taskify/app/modules/home/widgets/gradient_text.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class AriaView extends StatefulWidget {
+  const AriaView({super.key});
+
+  @override
+  State<AriaView> createState() => _AriaViewState();
+}
+
+class _AriaViewState extends State<AriaView> {
+  final TextEditingController _controller = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  late OpenAI? chatGPT;
+  bool _isImageSearch = false;
+
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    chatGPT = OpenAI.instance.build(
+      token: dotenv.env["API_KEY"],
+      baseOption: HttpSetup(
+        receiveTimeout: const Duration(milliseconds: 6000),
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    chatGPT?.close();
+    chatGPT?.genImgClose();
+    super.dispose();
+  }
+
+  // Link for api - https://beta.openai.com/account/api-keys
+
+  void _sendMessage() async {
+    if (_controller.text.isEmpty) return;
+    ChatMessage message = ChatMessage(
+      text: _controller.text,
+      sender: "user",
+      isImage: false,
+    );
+
+    setState(() {
+      _messages.insert(0, message);
+      _isTyping = true;
+    });
+
+    _controller.clear();
+
+    if (_isImageSearch) {
+      final request = GenerateImage(message.text, 1, size: "256x256");
+
+      final response = await chatGPT!.generateImage(request);
+      Vx.log(response!.data!.last!.url!);
+      insertNewData(response.data!.last!.url!, isImage: true);
+    } else {
+      final request = CompleteText(prompt: message.text, model: kTextDavinci3);
+
+      final response = await chatGPT!.onCompletion(request: request);
+      Vx.log(response!.choices[0].text);
+      insertNewData(response.choices[0].text, isImage: false);
+    }
+  }
+
+  void insertNewData(String response, {bool isImage = false}) {
+    ChatMessage botMessage = ChatMessage(
+      text: response,
+      sender: "bot",
+      isImage: isImage,
+    );
+
+    setState(() {
+      _isTyping = false;
+      _messages.insert(0, botMessage);
+    });
+  }
+
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _controller,
+            onSubmitted: (value) => _sendMessage(),
+            decoration: const InputDecoration.collapsed(
+              hintText: "Question/description",
+            ),
+          ),
+        ),
+        ButtonBar(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                _isImageSearch = false;
+                _sendMessage();
+              },
+            ),
+            TextButton(
+              onPressed: () {
+                _isImageSearch = true;
+                _sendMessage();
+              },
+              child: const Text("Generate Image"),
+            ),
+          ],
+        ),
+      ],
+    ).px16();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          color: Colors.orange[500],
+          onPressed: () => Navigator.pop(context),
+        ),
+        centerTitle: true,
+        title: GradientText(
+          "Aria",
+          style: GoogleFonts.poppins(
+              fontSize: 20.0.sp, fontWeight: FontWeight.w600),
+          gradient: LinearGradient(
+            colors: [
+              Colors.red.shade200,
+              Colors.pinkAccent,
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                reverse: true,
+                padding: Vx.m8,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
+              ),
+            ),
+            if (_isTyping) const ThreeDots(),
+            const Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: context.cardColor,
+              ),
+              child: _buildTextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
